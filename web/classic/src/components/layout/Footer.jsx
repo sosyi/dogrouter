55cac4d2fa61eb209ced7e3634e86b438d20f8ca
Chennovate International <188799@gmail.com>
2026-05-21 20:25:40 +1200

    Remove footer credit links to 'New API'
    Removed footer credit links to 'New API' from the layout.

--- web/classic/src/components/layout/Footer.jsx
+++ web/classic/src/components/layout/Footer.jsx
@@ -196,17 +196,7 @@ const FooterBar = () => {
           </div>
 
           <div className='text-sm'>
-            <span className='!text-semi-color-text-1'>
-              {t('设计与开发由')}{' '}
-            </span>
-            <a
-              href='https://github.com/QuantumNous/new-api'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='!text-semi-color-primary font-medium'
-            >
-              New API
-            </a>
+           
           </div>
         </div>
       </footer>
@@ -228,17 +218,7 @@ const FooterBar = () => {
               dangerouslySetInnerHTML={{ __html: footer }}
             ></div>
             <div className='text-sm flex-shrink-0'>
-              <span className='!text-semi-color-text-1'>
-                {t('设计与开发由')}{' '}
-              </span>
-              <a
-                href='https://github.com/QuantumNous/new-api'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='!text-semi-color-primary font-medium'
-              >
-                New API
-              </a>
+             
             </div>
           </div>
         </footer>

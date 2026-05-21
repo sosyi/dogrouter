50cbce207a39598e28c1f007097bbf78d0e443f1
Chennovate International <188799@gmail.com>
2026-05-21 20:28:58 +1200

    Update footer.tsx

--- web/default/src/components/layout/components/footer.tsx
+++ web/default/src/components/layout/components/footer.tsx
@@ -79,18 +79,7 @@ function ProjectAttribution(props: { currentYear: number }) {
 
   return (
     <div className='text-muted-foreground/45 text-center text-xs sm:text-right'>
-      <span className='text-muted-foreground/45'>
-        &copy; {props.currentYear}{' '}
-        <a
-          href='https://github.com/QuantumNous/new-api'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-foreground/70 hover:text-foreground font-medium transition-colors'
-        >
-          {t('New API')}
-        </a>
-        . {t(NEW_API_FOOTER_ATTRIBUTION_KEY)}
-      </span>
+     
     </div>
   )
 }
